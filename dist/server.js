329a449e67592620af2ee1900dd3685c1bb312ee
Artyom Ilyin <ilyin@infostorm.ru>
2022-07-10 13:25:15 +0300

    feat: appolo server

--- dist/server.js
+++ dist/server.js
@@ -4,13 +4,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const apollo_server_core_1 = require("apollo-server-core");
+const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const PORT = process.env.PORT || 3000;
-app.use('/', (req, res, next) => {
-    res.send({ message: 'Hello' });
-});
-app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
-});
+async function startApolloServer() {
+    const app = (0, express_1.default)();
+    app.use((0, cors_1.default)());
+    app.use(express_1.default.json());
+    const typeDefs = (0, apollo_server_express_1.gql) `
+        type Query {
+            greetings: String
+        }
+    `;
+    const resolvers = {};
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground],
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, path: '/graphql' });
+    const PORT = process.env.PORT || 3000;
+    app.use('/', (req, res, next) => {
+        res.send({ message: 'Hello' });
+    });
+    app.listen(PORT, () => {
+        console.log(`Server listening on PORT: ${PORT}`);
+        console.log(`Graphql Endpoint: ${apolloServer.graphqlPath}`);
+    });
+}
+startApolloServer();
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,40 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import cors from 'cors';
 import 'dotenv/config';
 
-const app = express();
-app.use(express.json());
+async function startApolloServer() {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+    const typeDefs = gql`
+        type Query {
+            greetings: String
+        }
+    `;
 
-app.use('/', (req, res, next) => {
-    res.send({ message: 'Hello' });
-});
+    const resolvers = {};
 
-app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
-});
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+    const PORT = process.env.PORT || 3000;
+
+    app.use('/', (req, res, next) => {
+        res.send({ message: 'Hello' });
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on PORT: ${PORT}`);
+        console.log(`Graphql Endpoint: ${apolloServer.graphqlPath}`);
+    });
+}
+
+startApolloServer();
